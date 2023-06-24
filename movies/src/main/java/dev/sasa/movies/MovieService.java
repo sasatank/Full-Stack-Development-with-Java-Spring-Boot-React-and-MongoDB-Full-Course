852a7fdacc3d7c777935e47e3dd7730fe111a452
sasatank <sasa.tankytank@gmail.com>
2023-06-24 11:06:35 +0900

    0116_movie_controller

--- movies/src/main/java/dev/sasa/movies/MovieService.java
+++ movies/src/main/java/dev/sasa/movies/MovieService.java
@@ -1,10 +1,12 @@
 package dev.sasa.movies;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +15,9 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie>  singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
